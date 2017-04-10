845750aaef2666f2d5e498c3bba263e70f0a94ec
Gregorio Massara <greg.massara@gmail.com>
2017-04-10 18:55:23 +0100

    4 Strings  added for scores 0-4 3-6 7-9 and 10 messages

--- app/src/main/java/com/example/android/twinpeaksquiz/MainActivity.java
+++ app/src/main/java/com/example/android/twinpeaksquiz/MainActivity.java
@@ -12,7 +12,6 @@ import android.widget.Toast;
 /**
  * This app displays the results of the Quiz Game about the TV Series "Twin Peaks"
  */
-
 public class MainActivity extends AppCompatActivity {
 
     // Global Variables and Correct Answers to the Quiz
@@ -35,15 +34,11 @@ public class MainActivity extends AppCompatActivity {
     public int totalQuestions = 10;
     public int totalCorrectAnswered = 0;
 
-
-
     /**
      * This Method Saves Data from Vertical to Landscape
-     *
      */
 
     // Override the onSaveInstanceState method, this will write the variables values to the Bundle
-
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
@@ -60,14 +55,9 @@ public class MainActivity extends AppCompatActivity {
         totalCorrectAnswered = savedInstanceState.getInt("totalCorrectAnsweredState");
         message = savedInstanceState.getString("messageState");
 
-
         /* Display values after restoring */
 
         displayMessage(message);
-
-
-
-
     }
 
     @Override
@@ -75,8 +65,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-
 
     /**
      * This Method Submits the Results of the Quiz
@@ -89,147 +77,108 @@ public class MainActivity extends AppCompatActivity {
         //Quiz 1 Answer 1
         RadioButton radioButton1 = (RadioButton) findViewById(R.id.Q1_R1);
         boolean quest1Answer1 = radioButton1.isChecked();
-
         //Quiz 1 Answer 2
         RadioButton radioButton2 = (RadioButton) findViewById(R.id.Q1_R2);
         boolean quest1Answer2 = radioButton2.isChecked();
-
         //Quiz 1 Answer 3
         RadioButton radioButton3 = (RadioButton) findViewById(R.id.Q1_R3);
         boolean quest1Answer3 = radioButton3.isChecked();
-
-
         //Quiz 2 Answer 1
         RadioButton Q2radioButton1 = (RadioButton) findViewById(R.id.Q2_R1);
         boolean quest2Answer1 = Q2radioButton1.isChecked();
-
         //Quiz 2 Answer 2
         RadioButton Q2radioButton2 = (RadioButton) findViewById(R.id.Q2_R2);
         boolean quest2Answer2 = Q2radioButton2.isChecked();
-
         //Quiz 2 Answer 3
         RadioButton Q2radioButton3 = (RadioButton) findViewById(R.id.Q2_R3);
         boolean quest2Answer3 = Q2radioButton3.isChecked();
-
-
         //Quiz 3 Answer 1
         CheckBox checkBox1 = (CheckBox) findViewById(R.id.checkbox1);
         boolean isGuilty1 = checkBox1.isChecked();
-
         //Quiz 3 Answer 2
         CheckBox checkBox2 = (CheckBox) findViewById(R.id.checkbox2);
         boolean isGuilty2 = checkBox2.isChecked();
-
         //Quiz 3 Answer 3
         CheckBox checkBox3 = (CheckBox) findViewById(R.id.checkbox3);
         boolean isGuilty3 = checkBox3.isChecked();
-
         //Quiz 3 Answer 4
         CheckBox checkBox4 = (CheckBox) findViewById(R.id.checkbox4);
         boolean isGuilty4 = checkBox4.isChecked();
-
         //Quiz 3 Answer 5
         CheckBox checkBox5 = (CheckBox) findViewById(R.id.checkbox5);
         boolean isGuilty5 = checkBox5.isChecked();
-
         //Quiz 3 Answer 6
         CheckBox checkBox6 = (CheckBox) findViewById(R.id.checkbox6);
         boolean isGuilty6 = checkBox6.isChecked();
-
         //Quiz 3 Answer 7
         CheckBox checkBox7 = (CheckBox) findViewById(R.id.checkbox7);
         boolean isGuilty7 = checkBox7.isChecked();
-
         //Quiz 3 Answer 8
         CheckBox checkBox8 = (CheckBox) findViewById(R.id.checkbox8);
         boolean isGuilty8 = checkBox8.isChecked();
-
-
         //Quiz 4 One Answer Only
         EditText population = (EditText) findViewById(R.id.population);
         String populationNumber = population.getText().toString();
-
         //Quiz 5 Answer 1
-        RadioButton Q5RadioButton1 =(RadioButton) findViewById(R.id.Q5_R1);
+        RadioButton Q5RadioButton1 = (RadioButton) findViewById(R.id.Q5_R1);
         Boolean quest5Answer1 = Q5RadioButton1.isChecked();
-
         //Quiz 5 Answer 2
-        RadioButton Q5RadioButton2 =(RadioButton) findViewById(R.id.Q5_R2);
+        RadioButton Q5RadioButton2 = (RadioButton) findViewById(R.id.Q5_R2);
         Boolean quest5Answer2 = Q5RadioButton2.isChecked();
-
         //Quiz 5 Answer 3
-        RadioButton Q5RadioButton3 =(RadioButton) findViewById(R.id.Q5_R3);
+        RadioButton Q5RadioButton3 = (RadioButton) findViewById(R.id.Q5_R3);
         Boolean quest5Answer3 = Q5RadioButton3.isChecked();
-
         //Quiz 6 Answer 1
-        RadioButton Q6RadioButton1 =(RadioButton) findViewById(R.id.Q6_R1);
+        RadioButton Q6RadioButton1 = (RadioButton) findViewById(R.id.Q6_R1);
         Boolean quest6Answer1 = Q6RadioButton1.isChecked();
-
         //Quiz 6 Answer 2
-        RadioButton Q6RadioButton2 =(RadioButton) findViewById(R.id.Q6_R2);
+        RadioButton Q6RadioButton2 = (RadioButton) findViewById(R.id.Q6_R2);
         Boolean quest6Answer2 = Q6RadioButton2.isChecked();
-
         //Quiz 6 Answer 3
-        RadioButton Q6RadioButton3 =(RadioButton) findViewById(R.id.Q6_R3);
+        RadioButton Q6RadioButton3 = (RadioButton) findViewById(R.id.Q6_R3);
         Boolean quest6Answer3 = Q6RadioButton3.isChecked();
-
         //Quiz 7 Answer 1
-        RadioButton Q7RadioButton1 =(RadioButton) findViewById(R.id.Q7_R1);
+        RadioButton Q7RadioButton1 = (RadioButton) findViewById(R.id.Q7_R1);
         Boolean quest7Answer1 = Q7RadioButton1.isChecked();
-
         //Quiz 7 Answer 2
-        RadioButton Q7RadioButton2 =(RadioButton) findViewById(R.id.Q7_R2);
+        RadioButton Q7RadioButton2 = (RadioButton) findViewById(R.id.Q7_R2);
         Boolean quest7Answer2 = Q7RadioButton2.isChecked();
-
         //Quiz 7 Answer 3
-        RadioButton Q7RadioButton3 =(RadioButton) findViewById(R.id.Q7_R3);
+        RadioButton Q7RadioButton3 = (RadioButton) findViewById(R.id.Q7_R3);
         Boolean quest7Answer3 = Q7RadioButton3.isChecked();
-
         //Quiz 8 Answer 1
-        RadioButton Q8RadioButton1 =(RadioButton) findViewById(R.id.Q8_R1);
+        RadioButton Q8RadioButton1 = (RadioButton) findViewById(R.id.Q8_R1);
         Boolean quest8Answer1 = Q8RadioButton1.isChecked();
-
         //Quiz 8 Answer 2
-        RadioButton Q8RadioButton2 =(RadioButton) findViewById(R.id.Q8_R2);
+        RadioButton Q8RadioButton2 = (RadioButton) findViewById(R.id.Q8_R2);
         Boolean quest8Answer2 = Q8RadioButton2.isChecked();
-
         //Quiz 8 Answer 3
-        RadioButton Q8RadioButton3 =(RadioButton) findViewById(R.id.Q8_R3);
+        RadioButton Q8RadioButton3 = (RadioButton) findViewById(R.id.Q8_R3);
         Boolean quest8Answer3 = Q8RadioButton3.isChecked();
-
         //Quiz 9 Answer 1
-        RadioButton Q9RadioButton1 =(RadioButton) findViewById(R.id.Q9_R1);
+        RadioButton Q9RadioButton1 = (RadioButton) findViewById(R.id.Q9_R1);
         Boolean quest9Answer1 = Q9RadioButton1.isChecked();
-
         //Quiz 9 Answer 2
-        RadioButton Q9RadioButton2 =(RadioButton) findViewById(R.id.Q9_R2);
+        RadioButton Q9RadioButton2 = (RadioButton) findViewById(R.id.Q9_R2);
         Boolean quest9Answer2 = Q9RadioButton2.isChecked();
-
         //Quiz 9 Answer 3
-        RadioButton Q9RadioButton3 =(RadioButton) findViewById(R.id.Q9_R3);
+        RadioButton Q9RadioButton3 = (RadioButton) findViewById(R.id.Q9_R3);
         Boolean quest9Answer3 = Q9RadioButton3.isChecked();
-
         //Quiz 10 Answer 1
-        RadioButton Q10RadioButton1 =(RadioButton) findViewById(R.id.Q10_R1);
+        RadioButton Q10RadioButton1 = (RadioButton) findViewById(R.id.Q10_R1);
         Boolean quest10Answer1 = Q10RadioButton1.isChecked();
-
         //Quiz 10 Answer 2
-        RadioButton Q10RadioButton2 =(RadioButton) findViewById(R.id.Q10_R2);
+        RadioButton Q10RadioButton2 = (RadioButton) findViewById(R.id.Q10_R2);
         Boolean quest10Answer2 = Q10RadioButton2.isChecked();
-
         //Quiz 10 Answer 3
-        RadioButton Q10RadioButton3 =(RadioButton) findViewById(R.id.Q10_R3);
+        RadioButton Q10RadioButton3 = (RadioButton) findViewById(R.id.Q10_R3);
         Boolean quest10Answer3 = Q10RadioButton3.isChecked();
 
-
-
-
+        //Calling the the 10 methods for Checking the Answers
         checkAnswer1(quest1Answer1, quest1Answer2, quest1Answer3);
         checkAnswer2(quest2Answer1, quest2Answer2, quest2Answer3);
         checkAnswer3(isGuilty1, isGuilty2, isGuilty3, isGuilty4, isGuilty5, isGuilty6, isGuilty7, isGuilty8);
         checkAnswer4(populationNumber);
-
-
         checkAnswer5(quest5Answer1, quest5Answer2, quest5Answer3);
         checkAnswer6(quest6Answer1, quest6Answer2, quest6Answer3);
         checkAnswer7(quest7Answer1, quest7Answer2, quest7Answer3);
@@ -237,12 +186,15 @@ public class MainActivity extends AppCompatActivity {
         checkAnswer9(quest9Answer1, quest9Answer2, quest9Answer3);
         checkAnswer10(quest10Answer1, quest10Answer2, quest10Answer3);
 
+        //Creating the Display Message for 0 to 4 correct answers
 
+        String message0to4correct = "\nPete Martell: She's dead... Wrapped in plastic. \nWatch again the series and try again!";
+        String message5to7correct = "\nSheriff Truman: I hear that you're real good at what you do.";
+        String message8to9correct = "\nDale Cooper: A little habit can provide a strong foundation.";
+        String messageAllcorrect = "Laura Palmer: Fire Walk with me!";
 
-
-
-
-        String message = "Total Score: " + totalCorrectAnswered + " out of " + totalQuestions + "\n";
+        String message = "You Scored: " + totalCorrectAnswered + " out of " + totalQuestions + "\n";
+        message += "\nWatch again the series and try again!";
         message += "\nQ1: " + message1;
         message += "\nQ2: " + message2;
         message += "\nQ3: " + message3;
@@ -255,17 +207,17 @@ public class MainActivity extends AppCompatActivity {
         message += "\nQ10 " + message10;
         displayMessage(message);
 
+
     }
 
     /**
-     * This Method checks whether Question 1 Answers are correct or not
+     * This Method checks whether Question 1 Answer is correct or not
      *
      * @param quest1Answer1 Answer 1
      * @param quest1Answer2 Answer 2
      * @param quest1Answer3 Answer 3
      * @return
      */
-
     public String checkAnswer1(boolean quest1Answer1, boolean quest1Answer2, boolean quest1Answer3) {
 
         if (quest1Answer1) {
@@ -281,7 +233,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * This Method checks whether Question 2 Answers are correct or not
+     * This Method checks whether Question 2 Answer correct or not
      *
      * @param quest2Answer1 Answer 1
      * @param quest2Answer2 Answer 2
@@ -318,7 +270,6 @@ public class MainActivity extends AppCompatActivity {
         return message3;
     }
 
-
     /**
      * This method checks whether Question 4 is correct
      */
@@ -334,16 +285,13 @@ public class MainActivity extends AppCompatActivity {
         return message4;
     }
 
-
     /**
-     * This Method checks whether Question 5 Answers are correct or not
+     * This Method checks whether Question 5 is correct or not
      *
      * @param quest5Answer1 Answer 1
      * @param quest5Answer2 Answer 2
      * @param quest5Answer3 Answer 3
-     * @return
      */
-
     public String checkAnswer5(boolean quest5Answer1, boolean quest5Answer2, boolean quest5Answer3) {
 
         if (quest5Answer1) {
@@ -354,12 +302,11 @@ public class MainActivity extends AppCompatActivity {
         } else if (quest5Answer3) {
             message5 = incorrect;
         }
-
         return message5;
     }
 
     /**
-     * This Method checks whether Question 1 Answers are correct or not
+     * This Method checks whether Question 1 Answer is correct or not
      *
      * @param quest6Answer1 Answer 1
      * @param quest6Answer2 Answer 2
@@ -377,19 +324,17 @@ public class MainActivity extends AppCompatActivity {
         } else if (quest6Answer3) {
             message6 = incorrect;
         }
-
         return message6;
     }
 
     /**
-     * This Method checks whether Question 7 Answers are correct or not
+     * This Method checks whether Question 7 Answer is correct or not
      *
      * @param quest7Answer1 Answer 1
      * @param quest7Answer2 Answer 2
      * @param quest7Answer3 Answer 3
      * @return
      */
-
     public String checkAnswer7(boolean quest7Answer1, boolean quest7Answer2, boolean quest7Answer3) {
 
         if (quest7Answer1) {
@@ -400,12 +345,11 @@ public class MainActivity extends AppCompatActivity {
             message7 = correct;
             totalCorrectAnswered++;
         }
-
         return message7;
     }
 
     /**
-     * This Method checks whether Question 8 Answers are correct or not
+     * This Method checks whether Question 8 Answer is correct or not
      *
      * @param quest8Answer1 Answer 1
      * @param quest8Answer2 Answer 2
@@ -423,19 +367,17 @@ public class MainActivity extends AppCompatActivity {
         } else if (quest8Answer3) {
             message8 = incorrect;
         }
-
         return message8;
     }
 
     /**
-     * This Method checks whether Question 9 Answers are correct or not
+     * This Method checks whether Question 9 Answer is correct or not
      *
      * @param quest9Answer1 Answer 1
      * @param quest9Answer2 Answer 2
      * @param quest9Answer3 Answer 3
      * @return
      */
-
     public String checkAnswer9(boolean quest9Answer1, boolean quest9Answer2, boolean quest9Answer3) {
 
         if (quest9Answer1) {
@@ -446,19 +388,17 @@ public class MainActivity extends AppCompatActivity {
         } else if (quest9Answer3) {
             message9 = incorrect;
         }
-
         return message9;
     }
 
     /**
-     * This Method checks whether Question 10 Answers are correct or not
+     * This Method checks whether Question 10 Answer is correct or not
      *
      * @param quest10Answer1 Answer 1
      * @param quest10Answer2 Answer 2
      * @param quest10Answer3 Answer 3
      * @return
      */
-
     public String checkAnswer10(boolean quest10Answer1, boolean quest10Answer2, boolean quest10Answer3) {
 
         if (quest10Answer1) {
@@ -469,13 +409,11 @@ public class MainActivity extends AppCompatActivity {
         } else if (quest10Answer3) {
             message10 = incorrect;
         }
-
         return message10;
     }
 
-
     /**
-     * This Method Displays the Result of The Quiz
+     * This Method Displays the Result message of The Quiz for 0 to 4 right answers
      *
      * @param message is the message of all the Answers
      */
@@ -484,20 +422,17 @@ public class MainActivity extends AppCompatActivity {
 
         Toast toast = Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG);
         toast.show();
-
     }
 
     /**
      * This Method Resets all the Answers
      */
-
     public void resetQuiz(View view) {
 
         //Reset the Score
         totalCorrectAnswered = 0;
 
         //Reset Messages
-
         message1 = "";
         message2 = "";
         message3 = "";
@@ -513,72 +448,54 @@ public class MainActivity extends AppCompatActivity {
         //Quiz 1 Reset
         RadioGroup radioGroup1 = (RadioGroup) findViewById(R.id.Answers1);
         radioGroup1.clearCheck();
-
         //Quiz 2 Reset
         RadioGroup radioGroup2 = (RadioGroup) findViewById(R.id.Answers2);
         radioGroup2.clearCheck();
-
         //Quiz 3 Answer 1 Reset
         CheckBox checkBox1 = (CheckBox) findViewById(R.id.checkbox1);
         checkBox1.setChecked(false);
-
         //Quiz 3 Answer 2 Reset
         CheckBox checkBox2 = (CheckBox) findViewById(R.id.checkbox2);
         checkBox2.setChecked(false);
-
         //Quiz 3 Answer 3 Reset
         CheckBox checkBox3 = (CheckBox) findViewById(R.id.checkbox3);
         checkBox3.setChecked(false);
-
         //Quiz 3 Answer 4 Reset
         CheckBox checkBox4 = (CheckBox) findViewById(R.id.checkbox4);
         checkBox4.setChecked(false);
-
         //Quiz 3 Answer 5 Reset
         CheckBox checkBox5 = (CheckBox) findViewById(R.id.checkbox5);
         checkBox5.setChecked(false);
-
         //Quiz 3 Answer 6 Reset
         CheckBox checkBox6 = (CheckBox) findViewById(R.id.checkbox6);
         checkBox6.setChecked(false);
-
         //Quiz 3 Answer 7 Reset
         CheckBox checkBox7 = (CheckBox) findViewById(R.id.checkbox7);
         checkBox7.setChecked(false);
-
         //Quiz 3 Answer 8 Reset
         CheckBox checkBox8 = (CheckBox) findViewById(R.id.checkbox8);
         checkBox8.setChecked(false);
-
         //Quiz 4 Reset
         EditText editText = (EditText) findViewById(R.id.population);
         editText.setText(null);
-
         //Quiz 5 Reset
         RadioGroup radioGroup5 = (RadioGroup) findViewById(R.id.Answers5);
         radioGroup5.clearCheck();
-
         //Quiz 6 Reset
         RadioGroup radioGroup6 = (RadioGroup) findViewById(R.id.Answers6);
         radioGroup6.clearCheck();
-
         //Quiz 7 Reset
         RadioGroup radioGroup7 = (RadioGroup) findViewById(R.id.Answers7);
         radioGroup7.clearCheck();
-
         //Quiz 8 Reset
         RadioGroup radioGroup8 = (RadioGroup) findViewById(R.id.Answers8);
         radioGroup8.clearCheck();
-
         //Quiz 9 Reset
         RadioGroup radioGroup9 = (RadioGroup) findViewById(R.id.Answers9);
         radioGroup9.clearCheck();
-
         //Quiz 10 Reset
         RadioGroup radioGroup10 = (RadioGroup) findViewById(R.id.Answers10);
         radioGroup10.clearCheck();
-
-
     }
 
 }
